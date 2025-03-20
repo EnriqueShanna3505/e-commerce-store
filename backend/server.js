49332490956024210f7a5943047b51c7621cfc3e
Enrique Shanna <enriqueshanna00@gmail.com>
2025-03-21 03:40:33 +0800

    Creating authentication token, add database using mongodb, and also using redis

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // allows you to parse incoming JSON data
+
 app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
