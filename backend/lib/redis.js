298af6b95ab6f2c7cb07f2df949dcc732cedec41
Enrique Shanna <enriqueshanna00@gmail.com>
2025-03-21 06:22:38 +0800

    created refresh token

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -2,4 +2,5 @@ import Redis from 'ioredis';
 import dotenv from 'dotenv';
 
 dotenv.config();
+//redis is to stored the refresh/access tokens
 export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
