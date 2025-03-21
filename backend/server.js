dd5adfa5143be06da75be0c201c3863ab99faa8f
Enrique Shanna <enriqueshanna00@gmail.com>
2025-03-22 07:26:06 +0800

    Fixed a few imports, modules and stripe.

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,7 @@ app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/coupons', couponRoutes);
 app.use('/api/payments', paymentRoutes);
+app.use('/api/analytics', analyticsRoutes);
 
 app.listen(PORT, () => {
   console.log('Server is running on http://localhost:' + PORT);
