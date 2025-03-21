2dae8e4c91e9218458dea2f5021f7440753f5192
Enrique Shanna <enriqueshanna00@gmail.com>
2025-03-21 09:27:46 +0800

    added route for products, added cloudinary for image upload

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -148,3 +148,6 @@ export const refreshToken = async (req, res) => {
     res.status(500).json({ message: 'Server error', error: error.message });
   }
 };
+
+//TODO implement get profile later
+//export const getProfile = async (req, res) => {}
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+
 import authRoutes from './routes/auth.route.js';
+import productRoutes from './routes/product.route.js';
+
 import { connectDB } from './lib/db.js';
 
 dotenv.config();
@@ -11,12 +14,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows you to parse incoming JSON data
 app.use(cookieParser());
+
 app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
 
 app.listen(PORT, () => {
   console.log('Server is running on http://localhost:' + PORT);
 
   connectDB();
 });
-
-//
