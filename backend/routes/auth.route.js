d0dd676b5732a795566586d6a3288d248165e2cf
Enrique Shanna <enriqueshanna00@gmail.com>
2025-03-22 08:17:51 +0800

    entire backend part, as of now

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -4,13 +4,15 @@ import {
   logout,
   signup,
   refreshToken,
+  getProfile,
 } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/logout', logout);
 router.post('/refresh-token', refreshToken);
-//router.get('/profile', protectRoute, getProfile);
+router.get('/profile', protectRoute, getProfile);
 
 export default router;
