b645289871306e2bc21dae3c536ea7cfcfa44c1c
Enrique Shanna <enriqueshanna00@gmail.com>
2025-03-24 22:15:41 +0800

    created admin page, analytics, testing cloudinary, adding products, and products page

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); // allows you to parse incoming JSON data
+app.use(express.json({ limit: '10mb' })); // allows you to parse incoming JSON data
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
