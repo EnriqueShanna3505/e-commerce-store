d4bd921c0c7a187858762681f7dd62ca66dbade9
Enrique Shanna <enriqueshanna00@gmail.com>
2025-03-24 18:05:56 +0800

    Make Login, SignUp Page interactive

--- frontend/e-commerce-store/src/pages/LoginPage.jsx
+++ frontend/e-commerce-store/src/pages/LoginPage.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
 import { useState } from 'react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { LogIn, Mail, Lock, ArrowRight, Loader } from 'lucide-react';
+import { useUserStore } from '../stores/useUserStore';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const loading = true;
+  const { login, loading } = useUserStore();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(email, password);
     login(email, password);
   };
+
   return (
     <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <motion.div
@@ -54,9 +55,9 @@ const LoginPage = () => {
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   className=" block w-full px-3 py-2 pl-10 bg-gray-700 border border-gray-600 
-              rounded-md shadow-sm
-               placeholder-gray-400 focus:outline-none focus:ring-emerald-500 
-               focus:border-emerald-500 sm:text-sm"
+									rounded-md shadow-sm
+									 placeholder-gray-400 focus:outline-none focus:ring-emerald-500 
+									 focus:border-emerald-500 sm:text-sm"
                   placeholder="you@example.com"
                 />
               </div>
@@ -80,7 +81,7 @@ const LoginPage = () => {
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   className=" block w-full px-3 py-2 pl-10 bg-gray-700 border border-gray-600 
-              rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm"
+									rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm"
                   placeholder="••••••••"
                 />
               </div>
@@ -89,9 +90,9 @@ const LoginPage = () => {
             <button
               type="submit"
               className="w-full flex justify-center py-2 px-4 border border-transparent 
-          rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600
-           hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2
-            focus:ring-emerald-500 transition duration-150 ease-in-out disabled:opacity-50"
+							rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600
+							 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2
+							  focus:ring-emerald-500 transition duration-150 ease-in-out disabled:opacity-50"
               disabled={loading}
             >
               {loading ? (
@@ -104,7 +105,7 @@ const LoginPage = () => {
                 </>
               ) : (
                 <>
-                  <Login className="mr-2 h-5 w-5" aria-hidden="true" />
+                  <LogIn className="mr-2 h-5 w-5" aria-hidden="true" />
                   Login
                 </>
               )}
@@ -117,7 +118,7 @@ const LoginPage = () => {
               to="/signup"
               className="font-medium text-emerald-400 hover:text-emerald-300"
             >
-              Login here <ArrowRight className="inline h-4 w-4" />
+              Sign up now <ArrowRight className="inline h-4 w-4" />
             </Link>
           </p>
         </div>
@@ -125,5 +126,4 @@ const LoginPage = () => {
     </div>
   );
 };
-
 export default LoginPage;
