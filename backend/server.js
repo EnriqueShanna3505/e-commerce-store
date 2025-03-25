0c1772773c6a3b5df9f4d1c97dc2971634fda527
Enrique Shanna <enriqueshanna00@gmail.com>
2025-03-26 03:48:07 +0800

    Configured some stuffs

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import authRoutes from './routes/auth.route.js';
 import productRoutes from './routes/product.route.js';
@@ -15,6 +16,7 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: '10mb' })); // allows you to parse incoming JSON data
 app.use(cookieParser());
@@ -25,6 +27,24 @@ app.use('/api/cart', cartRoutes);
 app.use('/api/coupons', couponRoutes);
 app.use('/api/payments', paymentRoutes);
 app.use('/api/analytics', analyticsRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(
+    express.static(path.join(__dirname, '/frontend/e-commerce-store/dist'))
+  );
+
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(
+        __dirname,
+        'frontend',
+        'e-commerce-store',
+        'dist',
+        'index.html'
+      )
+    );
+  });
+}
 
 app.listen(PORT, () => {
   console.log('Server is running on http://localhost:' + PORT);
